471606ce6c51127bfe16eacb75f6857ba29aface
JFKongphop <kongphopleo@gmail.com>
2024-07-16 22:15:16 +0700

    clear the some lib not use

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { Buffer } from 'buffer'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -9,11 +8,10 @@ import { config } from './wagmi.ts'
 
 import './index.css'
 
-globalThis.Buffer = Buffer
 
 const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
